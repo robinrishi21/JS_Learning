0a1fff35af114e464c69e811cfe52935332151b7
ROHIT RISHI <92050757+robinrishi21@users.noreply.github.com>
2023-10-30 17:25:47 +0000

    conversion Opeartion

---
+++ 01basics/03_conversionOperation.js
@@ -0,0 +1,47 @@
+let score = undefined;
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//score = "33";
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber);   //33
+
+//score = "abc"
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); //NaN
+
+//score = null
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); //0
+
+//score = undefined
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);  //number
+//console.log(valueInNumber);   //NaN
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1, false => 0
+
+//{To boolean}
+//let isLoggedIn = "";
+//let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false
+// "rohit" => true
+
+//{to string}
+let someNumber = 33;
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
+
+
+
