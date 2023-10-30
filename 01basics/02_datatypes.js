2d545770c4a90c3d4310f5c6dbf32447f19ed987
ROHIT RISHI <92050757+robinrishi21@users.noreply.github.com>
2023-10-30 15:58:44 +0000

    datatypes

---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";   //treat all JS code as newer version 
+
+//alert(3+3);  //we are using nodejs, not browser
+
+console.log(3
+    + 3);  //code readability should be high
+
+    console.log("Rohit");
+
+    //Datatypes
+    //Primitive
+    //1. number => 2 to power 53 range
+    //2. BigInt
+    //3. string => ""
+    //4. boolean => true/false
+    //5. undefined => which is not defined
+    //6. null => stand alone value
+    //7. symbol => unique (for uniqueness of components in react )
+
+    //Non-primitive
+    //object
+
+    console.log(typeof undefined);   //undefined
+    console.log(typeof null);      //object
+
